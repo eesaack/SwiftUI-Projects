38757c4ed82c14c5c4b1f61a88e0f7a630e188b1
Dilbar Isakova <geekwarns@gmail.com>
2022-09-20 23:25:56 +0600

    Project 1 - Creating and Combining Views

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by eesaack on 19.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
